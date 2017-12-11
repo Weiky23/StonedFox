da58af51f6029cb23cbe02412500393a5e39e9f3
Weiky23 <23mikha@gmail.com>
2017-12-11 20:42:06 +0300

    f

---
+++ Configurations/ScenesConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace StonedFox.Configurations
+{
+    [CreateAssetMenu(fileName = "ScenesConfiguration", menuName = "Ship Project/Scenes Configuration")]
+    public class ScenesConfiguration : ScriptableObject
+    {
+        public ScenePicker[] scenes;
+
+        public const string Scenes = "scenes";
+    }
+
+    [Serializable]
+    public class ScenePicker
+    {
+        public string scenePath;
+
+        public const string ScenePath = "scenePath";
+    }
+}
---
+++ Configurations/ScenesConfigurationEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace StonedFox.Configurations
+{
+    [CustomEditor(typeof(ScenesConfiguration))]
+    public class ScenesConfigurationEditor : Editor
+    {
+        SerializedProperty scenesProperty;
+
+        private void OnEnable()
+        {
+            scenesProperty = serializedObject.FindProperty(ScenesConfiguration.Scenes);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.LabelField("Scenes number", EditorStyles.boldLabel);
+            EditorGUI.BeginChangeCheck();
+            int count = EditorGUILayout.IntField(scenesProperty.arraySize);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                scenesProperty.arraySize = count;
+            }
+
+            EditorGUILayout.LabelField("Scenes order", EditorStyles.boldLabel);
+
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                SerializedProperty prop = scenesProperty.GetArrayElementAtIndex(i);
+                EditorGUI.BeginChangeCheck();
+                SerializedProperty sceneProp = prop.FindPropertyRelative(ScenePicker.ScenePath);
+                SceneAsset oldScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(sceneProp.stringValue);
+                SceneAsset newScene = EditorGUILayout.ObjectField(oldScene, typeof(SceneAsset), false) as SceneAsset;
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    string newPath = AssetDatabase.GetAssetPath(newScene);
+                    sceneProp.stringValue = newPath;
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+}
